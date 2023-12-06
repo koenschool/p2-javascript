a9d9de738ef7df2be7c728445cf055b00ccba2c8
koen <9023005@student.zadkine.nl>
2023-12-06 11:24:49 +0100

    opdracht4.2

---
+++ opdracht4.2/opdracht4_2.js
@@ -0,0 +1,13 @@
+
+
+
+
+function getal(){
+for (let i = 0; i < 100; i++) {
+    text1++;
+    text2=text1+"0"; 
+    text3 += text1 + " x 10 =" + text2 + "<br>";
+    document.getElementById("text").innerHTML = text3;
+    console.log("hello");
+}
+}
