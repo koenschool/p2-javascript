b91d49726c5f9ab4c3126132c942b53c25b75504
koen <9023005@student.zadkine.nl>
2023-12-06 10:48:30 +0100

    opdracht5.1

---
+++ opdracht5.1/opdracht5_1.js
@@ -0,0 +1,39 @@
+function steen(){
+    getal = Math.floor(Math.random() * 3 + 1);
+    if(getal==1){
+        document.getElementById("getal").innerHTML = "jij deed 🪨 en de tegenstander deed 🪨 dus het is gelijk";
+    }
+    else if(getal==2){
+        document.getElementById("getal").innerHTML =  "jij deed 🪨 en de tegenstander deed 📃 dus je hebt verloren";
+    }
+    else if(getal==3){
+        document.getElementById("getal").innerHTML =  "jij deed 🪨 en de tegenstander deed ✂️ dus je hebt gewonnen";
+    }
+ 
+}
+
+function papier(){
+    getal = Math.floor(Math.random() * 3 + 1);
+    if(getal==1){
+        document.getElementById("getal").innerHTML = "jij  deed 📃 en de tegenstander deed 📃 dus het is gelijk";
+    }
+    else if(getal==2){
+        document.getElementById("getal").innerHTML =  "jij  deed 📃 en de tegenstander deed ✂️ dus je hebt verloren";
+    }
+    else if(getal==3){
+        document.getElementById("getal").innerHTML =  "jij  deed 📃 en de tegenstander deed 🪨 dus je hebt gewonnen";
+    }
+}
+
+function schaar(){
+    getal = Math.floor(Math.random() * 3 + 1);
+    if(getal==1){
+        document.getElementById("getal").innerHTML = "jij  deed ✂️ en de tegenstander deed ✂️ dus het is gelijk";
+    }
+    else if(getal==2){
+        document.getElementById("getal").innerHTML =  "jij  deed ✂️ en de tegenstander deed 🪨 dus je hebt verloren";
+    }
+    else if(getal==3){
+        document.getElementById("getal").innerHTML =  "jij  deed ✂️ en de tegenstander deed 📃 dus je hebt gewonnen";
+    }
+}
